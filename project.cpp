15885c82985dc9bb7f7ad6c4b43e5590b424fa50
Nshm <nargeshm1378@gmail.com>
2018-02-01 23:53:26 +0330

    gift functon
    gift func works probable(except colorkey)

--- project.cpp
+++ project.cpp
@@ -67,13 +67,22 @@ struct ammo
 SDL_Surface * load_image (std::string file);
 void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination );
 void gift_motion (gft * gift ,int n,SDL_Surface * screen);
+void gifte(gft *gift,int n, SDL_Surface *screen);
 int main()
 {	
 	SDL_Surface * screen=NULL;
 	int bgX = 0, bgy = 0;
 	screen=SDL_SetVideoMode( 450, 700, 32, 0 );
 	SDL_Surface * picture;
-	
+	gft gift[5]		;
+	for(int i=0;i<5;i++)
+	{
+		gift[i].yp=0;
+		gift[i].xp=10+(10*i);
+		gift[i].vy=i+1;
+		gift[i].state=2;
+		gift[i].surface=load_image("gift0.png");
+	}
 	picture=load_image("stars1.jpg");
 	
 	while(1)
@@ -85,7 +94,7 @@ int main()
 		}
 	  	apply_surface( bgX, bgy, picture, screen ); 
       	apply_surface( bgX , bgy-picture->h, picture, screen );
-    
+    	gifte(&gift[0],5, screen);
 		SDL_Flip(screen);
 		SDL_Delay(20);
 	}
@@ -107,8 +116,8 @@ void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination 
     offset.y = y;
     SDL_BlitSurface( source, NULL, destination, &offset );
 }
-void gift_motion (gft * gift,int n,SDL_Surface * screen )
-void gift(gft * gift,int n,SDL_Surface * screen )
+//void gift_motion (gft * gift,int n,SDL_Surface * screen )
+void gifte(gft * gift,int n,SDL_Surface * screen )
 {
 	for(int i=0 ; i<n ; i++)
 	{
@@ -145,4 +154,4 @@ void gift(gft * gift,int n,SDL_Surface * screen )
 		}
 	}
 	
-}
+}//hi
