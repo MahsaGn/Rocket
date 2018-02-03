2005f519288abeca7f0dba951722352e2be1eee6
mahsa <mahsa.ghaderan@gmail.com>
2018-02-03 16:28:15 +0330

    done rocket motion

--- backups/11.12.22.cpp
+++ backups/11.12.22.cpp
@@ -5,17 +5,15 @@
 #include<SDL/SDL_ttf.h>
 #include<SDL/SDL_gfxPrimitives.h>
 #include <string>
-#include<vector>
 using namespace std;
-struct rckt
+struct rocket
 {
-	int xp=225+10;
-	int yp=700;
-	int ymax=600;
-	float vx=2;
-	float vy=4;
-	int health=3;
-	int shotnum=1;
+	int xp;
+	int yp;
+	int ymax;
+	float vx;
+	float vy;
+	int health;
 	SDL_Surface * surface;
 };
 struct oppRocket
@@ -69,27 +67,15 @@ struct ammo
 SDL_Surface * load_image (std::string file);
 void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination );
 void gift_motion (gft * gift ,int n,SDL_Surface * screen);
-void gifte(gft *gift,int n, SDL_Surface *screen);
 int main()
-{
-	SDL_Event event;	
+{	
 	SDL_Surface * screen=NULL;
 	int bgX = 0, bgy = 0;
 	screen=SDL_SetVideoMode( 450, 700, 32, 0 );
 	SDL_Surface * picture;
-	gft gift[5]		;
-	for(int i=0;i<5;i++)
-	{
-		gift[i].yp=0;
-		gift[i].xp=10+(10*i);
-		gift[i].vy=i+1;
-		gift[i].state=2;
-		gift[i].surface=load_image("gift0.png");
-	}
+	
 	picture=load_image("stars1.jpg");
-	vector <ammo> shot;
-	rckt rocket;
-	rocket.surface=;
+	
 	while(1)
 	{
 		bgy+=2;
@@ -99,7 +85,7 @@ int main()
 		}
 	  	apply_surface( bgX, bgy, picture, screen ); 
       	apply_surface( bgX , bgy-picture->h, picture, screen );
-    	gifte(&gift[0],5, screen);
+    
 		SDL_Flip(screen);
 		SDL_Delay(20);
 	}
@@ -121,7 +107,6 @@ void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination 
     offset.y = y;
     SDL_BlitSurface( source, NULL, destination, &offset );
 }
-//void gift_motion (gft * gift,int n,SDL_Surface * screen )
 void gift(gft * gift,int n,SDL_Surface * screen )
 {
 	for(int i=0 ; i<n ; i++)
